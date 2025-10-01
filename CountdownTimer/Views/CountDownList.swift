cf97a16fb141b49ef8aad891e228c4ec6f14c32c
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-10-01 19:24:32 -0400

    skeleton for form

--- CountdownTimer/Views/CountDownList.swift
+++ CountdownTimer/Views/CountDownList.swift
@@ -1,7 +1,18 @@
 import SwiftUI
 
 struct CountDownList: View {
-    var body: some View {
+  // list of the count downs
+  // toolbars (add List, edit)
+  // addlist brings up sheet
+  @State private var isPresented = false
+  @State private var name = ""
+  @State private var date: Date = Date()
+  @State private var image: Data? = nil
+  var body: some View {
         Text("CountDownList page")
     }
+}
+
+#Preview {
+  CountDownList()
 }
--- CountdownTimer/Views/Tabs.swift
+++ CountdownTimer/Views/Tabs.swift
@@ -1,22 +1,12 @@
 import SwiftUI
 
 struct Tabs: View {
-  @State private var isPresented = false
     var body: some View {
         TabView {
             Tab("Home", systemImage: "timer") {
                 Home()
             }
-            Tab("New Countdown", systemImage: "plus.circle") {
-              Button(action: {isPresented.toggle()}) {
-                Text("Add new Countdown")
-                  .sheet(isPresented: $isPresented ) {
-                    NewCountDown()
-                  }
-              }
-                // add sheet here
-            }
-            Tab("All Timers", systemImage: "list.bullet.indent") {
+          Tab("All Timers", systemImage: "list.bullet.indent") {
                 CountDownList()
             }
         }
