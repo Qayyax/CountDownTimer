23d633d2a4095cd177b8df4737eee8561c3337d8
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-10-01 19:02:00 -0400

    class and struct for form

---
+++ CountdownTimer/CountDownForm.swift
@@ -0,0 +1,32 @@
+//
+//  CountDownForm.swift
+//  CountdownTimer
+//
+//  Created by Qayyax on 10/1/25.
+//
+
+import Foundation
+import SwiftUI
+import Observation
+
+struct CountDownType: Identifiable {
+  var id = UUID()
+  var name: String
+  var date: Date
+  var image: Data?
+}
+
+@Observable
+class CountDownClass {
+  var countDowns: [CountDownType] = []
+  func addCountDown(countDown: CountDownType) {
+    countDowns.append(countDown)
+  }
+  func removeCountDownFromList(atOffsets offsets: IndexSet) {
+    countDowns.remove(atOffsets: offsets)
+  }
+  
+  func deleteCountDown(_ countDown: CountDownType) {
+    countDowns.removeAll { $0.id == countDown.id }
+  }
+}
