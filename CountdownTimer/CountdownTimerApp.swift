18b04526c18cf06e303286ed0d24b5fa23d36ec9
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-10-01 17:56:57 -0400

    Initial Commit

---
+++ CountdownTimer/CountdownTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownTimerApp.swift
+//  CountdownTimer
+//
+//  Created by Qayyax on 10/1/25.
+//
+
+import SwiftUI
+
+@main
+struct CountdownTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
