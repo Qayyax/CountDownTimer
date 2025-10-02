f88d3b3310d2b89444796f625a6180c8a328e898
Qayyax <106759130+Qayyax@users.noreply.github.com>
2025-10-01 22:42:18 -0400

    moved to New count down

--- CountdownTimer/Views/CountDownList.swift
+++ CountdownTimer/Views/CountDownList.swift
@@ -4,25 +4,11 @@ struct CountDownList: View {
   // list of the count downs
   // toolbars (add List, edit)
   // addlist brings up sheet
-  @State private var isPresented = false
-  @State private var name = ""
-  @State private var date = Date.now
-  @State private var image: Data? = nil
-  // have to move this to NewCountDown.swift
   var body: some View {
-    VStack {
-      HStack{
-        Text("Event Name:")
-        TextField("Enter the name of the event", text: $name)
-      }
-      DatePicker(selection: $date,
-                 in: Date()..., // starts from today till no end
-                 displayedComponents: [.date, .hourAndMinute]) {
-        Text("Enter the date and time of the event")
-      }
-    }
+    Text("Hello world")
   }
 }
+
 
 #Preview {
   CountDownList()
--- CountdownTimer/Views/NewCountDown.swift
+++ CountdownTimer/Views/NewCountDown.swift
@@ -8,8 +8,23 @@
 import SwiftUI
 
 struct NewCountDown: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var isPresented = false
+  @State private var name = ""
+  @State private var date = Date.now
+  @State private var image: Data? = nil
+
+  var body: some View {
+    VStack {
+      HStack{
+        Text("Event Name:")
+        TextField("Enter the name of the event", text: $name)
+      }
+      DatePicker(selection: $date,
+                 in: Date()..., // starts from today till no end
+                 displayedComponents: [.date, .hourAndMinute]) {
+        Text("Enter the date and time of the event")
+      }
+    }
       // form to get user information
     }
 }
